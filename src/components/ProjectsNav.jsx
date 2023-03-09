bc1bf88a44cb964d9adbbdb9048a36f2577f809f
Nigel Nkomo <mrnbnkomo@gmail.com>
2023-03-09 10:31:56 +0100

    create ProjectsNav.jsx

---
+++ src/components/ProjectsNav.jsx
@@ -0,0 +1,14 @@
+export default function ProjectsNav() {
+  return (
+    <>
+      <div className="p-nav">
+        <div className="p-nav__left">
+          <a className="p-nav__btn--left">Prev</a>
+        </div>
+        <div className="p-nav__right">
+          <a className="p-nav__btn--right">Next</a>
+        </div>
+      </div>
+    </>
+  );
+}
